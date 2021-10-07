753df48583c14755b4a091f60837277bc6e5faa9
Bryan Daniel <brdaniel117@gmail.com>
2021-10-06 19:47:29 -0700

    updated work

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -9,43 +9,43 @@ const Work = () => {
                 </div>
                 {/*Large tile for most recent work*/}
                 <div className="section-content box-column">
-                    <a href="https://azbdaniel.github.io/run-buddy/" className="tile large-tile box-row">
+                    <a href="https://github.com/AZBDaniel/mernlin-pizza" className="tile large-tile box-row">
                         {/*Image Info Tag*/}
                         <div className="tile-tag">
-                            <p className="tile-title">Run Buddy</p>
-                            <p className="tile-info">HTML/CSS/JS</p>
+                            <p className="tile-title">MERNlin's Pizza</p>
+                            <p className="tile-info">HTML/CSS/JS/MERN Stack/GraphQL</p>
                         </div>
                     </a>
                     {/*Small tiles sections below*/}
                     <div className="box-row">
-                        <a href="https://azbdaniel.github.io/horiseon-site/" className="tile small-tile t1 box-row">
-                            {/*Image Info Tag*/}
-                            <div className="tile-tag">
-                                <p className="tile-title">Horiseon</p>
-                                <p className="tile-info">HTML/CSS</p>
-                            </div>
-                        </a>
-                        <a href="https://azbdaniel.github.io/hot-spot/" className="tile small-tile t2 box-row">
-                            <div className="tile-tag">
-                                {/*Image Info Tag*/}
-                                <p className="tile-title">Hot-Spot</p>
-                                <p className="tile-info">HTML, JS, CSS, Bulma, Google API, Weather API ?</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="box-row">
-                        <a href="https://github.com/AZBDaniel/mustache_cars" className="tile small-tile t3 box-row">
+                        <a href="https://github.com/AZBDaniel/mustache_cars" className="tile small-tile t1 box-row">
                             {/*Image Info Tag*/}
                             <div className="tile-tag">
                                 <p className="tile-title">Mustache-Cars</p>
                                 <p className="tile-info">Javascript, My SQL, Node.js, npm i, css</p>
                             </div>
                         </a>
-                        <a href className="tile small-tile t4 box-row">
+                        <a href="https://azbdaniel.github.io/hot-spot/" className="tile small-tile t2 box-row">
+                            <div className="tile-tag">
+                                {/*Image Info Tag*/}
+                                <p className="tile-title">Hot-Spot</p>
+                                <p className="tile-info">HTML, JS, CSS, Bulma, Google API</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box-row">
+                        <a href="https://github.com/AZBDaniel" className="tile small-tile t3 box-row">
+                            {/*Image Info Tag*/}
+                            <div className="tile-tag">
+                                <p className="tile-title">Project 4</p>
+                                <p className="tile-info">Langauges</p>
+                            </div>
+                        </a>
+                        <a href="https://github.com/AZBDaniel" className="tile small-tile t4 box-row">
                             {/*Image Info Tag*/}
                             <div className="tile-tag">
                                 <p className="tile-title">Project 5</p>
-                                <p className="tile-info">Langauges ?</p>
+                                <p className="tile-info">Langauges</p>
                             </div>
                         </a>
                     </div>
